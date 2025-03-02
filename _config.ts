45d8d391e00688aa2bb26859a2a044c785332e9f
藍+85CD <50108258+kwaa@users.noreply.github.com>
2025-03-02 22:35:39 +0800

    refactor: update config

--- _config.ts
+++ _config.ts
@@ -3,4 +3,23 @@ import blog from 'blog/mod.ts'
 
 export default lume({
   src: './src',
-}).use(blog())
+})
+  .use(blog())
+  // remove /posts prefix
+  // https://lume.land/docs/core/processors/#preprocess
+  .preprocess(['.html'], pages => {
+    for (const page of pages) {
+      if (page.data.url.startsWith('/posts')) {
+        page.data.url = page.data.url.slice('/posts'.length)
+      }
+    }
+  })
+  // remove author page
+  // https://lume.land/docs/core/processors/#remove-pages-dynamically
+  .process(['.html'], (filteredPages, allPages) => {
+    for (const page of filteredPages) {
+      if (page.data.url.startsWith('/author')) {
+        allPages.splice(allPages.indexOf(page), 1)
+      }
+    }
+  })
